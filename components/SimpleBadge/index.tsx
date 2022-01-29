3bd15b9888d8a0ae635a3ea49425e2c10f85980d
john <john@jjwheatley.com>
2022-01-29 09:46:37 +0100

    Stories for Storybook

--- components/SimpleBadge/index.tsx
+++ components/SimpleBadge/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { BadgeProps } from "./types";
-import css from "./styles.scss";
+import * as css from "./styles.scss";
 
 const SimpleBadge: FC<BadgeProps> = ({ content }) => (
   <div className={css.wrapper}>{content}</div>
